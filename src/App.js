ca19eb8f27a7bb70987caa62f7313d4917cb5b3f
marekwino23 <marekw1996@gmail.com>
2021-08-10 11:46:13 +0200

    initial app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">s
+     <div class="layout">
+      <h1>Rick and Morty</h1>
+      <img src={`${process.env.PUBLIC_URL}/rick-and-morty-portal-shoes-white-clothing-zavvi-23.png`} width="55%" alt="photo" />
+     </div>
+     <div className="box">
+      <h2>Search for characters</h2>
+      <form>
+        <input type="text" required placeholder="Name*"></input>
+        <br></br>
+        <br></br>
+        <input type="text" placeholder="Status"></input>
+        <br></br>
+        <br></br>
+        <input type="text" placeholder="Gender"></input>
+        <br></br>
+        <br></br>
+        <button>Search</button>
+      </form>
+     </div>
     </div>
   );
 }
